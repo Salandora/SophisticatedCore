f6d4fb010ed8d54c20c6a97ad32f72ad027b15cf
Salandora <salandora@gmail.com>
2024-03-18 18:46:24 +0100

    [Improvement]: Cherry picked and combined all build and github action changes

---
+++ src/main/java/net/p3pp3rf1y/sophisticatedcore/extensions/client/gui/screens/inventory/package-info.java
@@ -0,0 +1,10 @@
+
+/**
+ * Implementation code for SophisticatedCore.
+ */
+@ParametersAreNonnullByDefault @MethodsReturnNonnullByDefault
+package net.p3pp3rf1y.sophisticatedcore.extensions.client.gui.screens.inventory;
+
+import net.minecraft.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
