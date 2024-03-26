89ac63dcb99c4c25cab04d68af190bae0178f6da
Salandora <salandora@gmail.com>
2024-03-26 21:39:57 +0100

    [Fix]: Add BlockStateExtensions and ItemStackExtensions, so we don't need to pull porting_lib base in.

--- src/main/java/net/p3pp3rf1y/sophisticatedcore/mixin/common/BlockStateMixin.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedcore/mixin/common/BlockStateMixin.java
@@ -2,9 +2,10 @@ package net.p3pp3rf1y.sophisticatedcore.mixin.common;
 
 import org.spongepowered.asm.mixin.Mixin;
 
+import io.github.fabricators_of_create.porting_lib.extensions.BlockStateExtensions;
 import net.minecraft.world.level.block.state.BlockState;
 import net.p3pp3rf1y.sophisticatedcore.extensions.block.SophisticatedBlockState;
 
 @Mixin(BlockState.class)
-public class BlockStateMixin implements SophisticatedBlockState {
+public class BlockStateMixin implements SophisticatedBlockState, BlockStateExtensions {
 }
