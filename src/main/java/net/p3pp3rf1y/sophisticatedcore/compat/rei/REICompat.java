e599d72704c95d3d314359c6798f7491ff480ac3
Salandora <salandora@gmail.com>
2024-04-12 17:56:56 +0200

    [Fix]: Fixed a crash bug that cause a server environment with REI to crash upon start

---
+++ src/main/java/net/p3pp3rf1y/sophisticatedcore/compat/rei/REICompat.java
@@ -0,0 +1,17 @@
+package net.p3pp3rf1y.sophisticatedcore.compat.rei;
+
+import net.p3pp3rf1y.sophisticatedcore.compat.ICompat;
+import net.p3pp3rf1y.sophisticatedcore.compat.common.SetGhostSlotMessage;
+import net.p3pp3rf1y.sophisticatedcore.compat.common.SetMemorySlotMessage;
+import net.p3pp3rf1y.sophisticatedcore.network.PacketHandler;
+
+public class REICompat implements ICompat {
+	public REICompat() {
+	}
+
+	@Override
+	public void setup() {
+		PacketHandler.registerC2SMessage(SetGhostSlotMessage.class, SetGhostSlotMessage::new);
+		PacketHandler.registerC2SMessage(SetMemorySlotMessage.class, SetMemorySlotMessage::new);
+	}
+}
