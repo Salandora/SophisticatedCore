596ae752a2fe9bb706ba454041ae0d930cb503cb
Salandora <salandora@gmail.com>
2024-04-21 17:17:43 +0200

    [Fix]: Make accessors mutable

--- src/main/java/net/p3pp3rf1y/sophisticatedcore/mixin/common/accessor/SlotAccessor.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedcore/mixin/common/accessor/SlotAccessor.java
@@ -1,6 +1,7 @@
 package net.p3pp3rf1y.sophisticatedcore.mixin.common.accessor;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.gen.Accessor;
 import org.spongepowered.asm.mixin.gen.Invoker;
 
@@ -8,9 +9,11 @@ import net.minecraft.world.inventory.Slot;
 
 @Mixin(Slot.class)
 public interface SlotAccessor {
+	@Mutable
 	@Accessor("x")
 	void setX(int value);
 
+	@Mutable
 	@Accessor("y")
 	void setY(int value);
 
