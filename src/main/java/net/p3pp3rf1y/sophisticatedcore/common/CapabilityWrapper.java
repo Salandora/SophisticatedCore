c32cc407c734f8e71705ecfe0faf1b2225991049
Salandora <salandora@gmail.com>
2024-05-04 22:14:11 +0200

    [Improvement]: Move Storage Wrappers to SophisticatedCore for better compatibility between soph mods

--- src/main/java/net/p3pp3rf1y/sophisticatedcore/common/CapabilityWrapper.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedcore/common/CapabilityWrapper.java
@@ -1,9 +1,29 @@
 package net.p3pp3rf1y.sophisticatedcore.common;
 
+import net.fabricmc.fabric.api.lookup.v1.item.ItemApiLookup;
 import net.fabricmc.fabric.api.transfer.v1.item.ItemStorage;
+import net.minecraft.world.item.ItemStack;
+import net.p3pp3rf1y.sophisticatedcore.SophisticatedCore;
+import net.p3pp3rf1y.sophisticatedcore.api.IStorageWrapper;
 import net.p3pp3rf1y.sophisticatedcore.controller.ControllerBlockEntityBase;
 
+import java.util.Optional;
+
 public class CapabilityWrapper {
+	public static final ItemApiLookup<IStorageWrapper, Void> STORAGE_WRAPPER_CAPABILITY = ItemApiLookup.get(SophisticatedCore.getRL("storage_wrapper"), IStorageWrapper.class, Void.class);
+
+	public static Optional<IStorageWrapper> get(ItemStack provider) {
+		return get(provider, IStorageWrapper.class);
+	}
+
+	public static <T> Optional<T> get(ItemStack provider, Class<T> clazz) {
+		IStorageWrapper wrapper = STORAGE_WRAPPER_CAPABILITY.find(provider, null);
+		if (clazz.isInstance(wrapper)) {
+			return Optional.of(clazz.cast(wrapper));
+		}
+		return Optional.empty();
+	}
+
     public static void register() {
         ItemStorage.SIDED.registerFallback((level, pos, state, entity, dir) -> {
             if (entity instanceof ControllerBlockEntityBase) {
