2afde814276ece0597faa473055f0200706545b4
Salandora <salandora@gmail.com>
2024-05-13 23:56:28 +0200

    [Fix]: Reworked Litematica compat again due to reverting the Storage Wrapper move commits

--- src/main/java/net/p3pp3rf1y/sophisticatedcore/compat/litematica/LitematicaCompat.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedcore/compat/litematica/LitematicaCompat.java
@@ -3,15 +3,24 @@ package net.p3pp3rf1y.sophisticatedcore.compat.litematica;
 import me.pepperbell.simplenetworking.S2CPacket;
 
 import net.fabricmc.fabric.api.lookup.v1.item.ItemApiLookup;
+import net.minecraft.world.item.ItemStack;
 import net.p3pp3rf1y.sophisticatedcore.SophisticatedCore;
+import net.p3pp3rf1y.sophisticatedcore.api.IStorageWrapper;
 import net.p3pp3rf1y.sophisticatedcore.compat.ICompat;
 
-import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 import java.util.function.Function;
 
 public class LitematicaCompat implements ICompat {
-	public static final ItemApiLookup<Function<List<UUID>, S2CPacket>, UUID> REQUEST_CONTENTS_CAPABILITY = ItemApiLookup.get(SophisticatedCore.getRL("sophisticatedcore_requestcontents"), (Class<Function<List<UUID>, S2CPacket>>)(Class<?>) Function.class, UUID.class);
+	public record LitematicaWrapper(IStorageWrapper wrapper, Function<UUID, S2CPacket> packetGenerator) {
+	}
+
+	public static final ItemApiLookup<LitematicaWrapper, Void> LITEMATICA_CAPABILITY = ItemApiLookup.get(SophisticatedCore.getRL("sophisticatedcore_requestcontents"), LitematicaWrapper.class, Void.class);
+
+	public static Optional<LitematicaWrapper> getWrapper(ItemStack provider) {
+		return Optional.ofNullable(LITEMATICA_CAPABILITY.find(provider, null));
+	}
 
 	@Override
 	public void setup() {
