a670e20c3c966e5b13576382ef92555a058b6caf
Salandora <salandora@gmail.com>
2024-11-16 20:11:26 +0100

    [Fix]: Item counts in litematica were not properly showing

--- src/main/java/net/p3pp3rf1y/sophisticatedcore/compat/litematica/mixin/MaterialListUtilsMixin.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedcore/compat/litematica/mixin/MaterialListUtilsMixin.java
@@ -17,10 +17,16 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(MaterialListUtils.class)
 public class MaterialListUtilsMixin {
-	@Inject(method = "getInventoryItemCounts", at = @At(value = "INVOKE", target = "Lit/unimi/dsi/fastutil/objects/Object2IntOpenHashMap;addTo(Ljava/lang/Object;I)I", ordinal = 0, shift = At.Shift.AFTER, remap = false))
-	private static void sophisticatedCore$injectStorageBlockBase(Container inv, CallbackInfoReturnable<Object2IntOpenHashMap<ItemType>> cir,
-			@Local(ordinal = 0) Object2IntOpenHashMap<ItemType> map, @Local ItemStack stack) {
+	@Inject(method="getInventoryItemCounts", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;getItem()Lnet/minecraft/world/item/Item;"))
+	private static void sophisticatedCore$injectAdditionalChecks(Container inv, CallbackInfoReturnable<Object2IntOpenHashMap<ItemType>> cir,
+			@Local Object2IntOpenHashMap<ItemType> map, @Local ItemStack stack) {
 		LitematicaCompat.getWrapper(stack).ifPresent(litematicaWrapper -> sophisticatedCore$processItemStack(map, litematicaWrapper.wrapper()));
+	}
+
+	@Inject(method="getStoredItemCounts", at = @At(value = "INVOKE", target = "Lit/unimi/dsi/fastutil/objects/Object2IntOpenHashMap;addTo(Ljava/lang/Object;I)I"))
+	private static void sophisticatedCore$injectAdditionalChecks(ItemStack stackShulkerBox, CallbackInfoReturnable<Object2IntOpenHashMap<ItemType>> cir,
+			@Local Object2IntOpenHashMap<ItemType> map, @Local(ordinal = 1) ItemStack boxStack) {
+		LitematicaCompat.getWrapper(boxStack).ifPresent(litematicaWrapper -> sophisticatedCore$processItemStack(map, litematicaWrapper.wrapper()));
 	}
 
 	@Unique
