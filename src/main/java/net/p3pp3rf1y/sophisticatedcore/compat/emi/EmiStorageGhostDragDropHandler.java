3de26033ab92f9b44b16242a3814069f4795a545
Salandora <salandora@gmail.com>
2024-12-15 15:57:41 +0100

    [Fix]: Setting fluid filters with emi now works

--- src/main/java/net/p3pp3rf1y/sophisticatedcore/compat/emi/EmiStorageGhostDragDropHandler.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedcore/compat/emi/EmiStorageGhostDragDropHandler.java
@@ -1,39 +1,108 @@
 package net.p3pp3rf1y.sophisticatedcore.compat.emi;
 
+import com.google.common.collect.Maps;
+import dev.emi.emi.api.stack.EmiIngredient;
+import dev.emi.emi.api.stack.FluidEmiStack;
+import dev.emi.emi.api.stack.ItemEmiStack;
+import dev.emi.emi.api.widget.Bounds;
+import dev.emi.emi.mixin.accessor.HandledScreenAccessor;
+import dev.emi.emi.runtime.EmiDrawContext;
+import io.github.fabricators_of_create.porting_lib.fluids.FluidStack;
+import net.fabricmc.fabric.api.transfer.v1.fluid.FluidStorage;
+import net.fabricmc.fabric.api.transfer.v1.fluid.FluidVariant;
+import net.fabricmc.fabric.api.transfer.v1.storage.StorageUtil;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.material.Fluid;
 import net.p3pp3rf1y.sophisticatedcore.client.gui.StorageScreenBase;
+import net.p3pp3rf1y.sophisticatedcore.client.gui.utils.Position;
 import net.p3pp3rf1y.sophisticatedcore.common.gui.IFilterSlot;
 import net.p3pp3rf1y.sophisticatedcore.compat.jei.SetGhostSlotPayload;
 import net.p3pp3rf1y.sophisticatedcore.network.PacketDistributor;
 import dev.emi.emi.api.EmiDragDropHandler;
 import dev.emi.emi.api.stack.EmiStack;
+import net.p3pp3rf1y.sophisticatedcore.upgrades.pump.PumpUpgradeTab;
+import net.p3pp3rf1y.sophisticatedcore.util.CapabilityHelper;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
+import java.util.function.Consumer;
+import java.util.function.Function;
 
-public class EmiStorageGhostDragDropHandler<T extends StorageScreenBase<?>> extends EmiDragDropHandler.SlotBased<T> {
+public class EmiStorageGhostDragDropHandler<T extends StorageScreenBase<?>> implements EmiDragDropHandler<T> {
+    private final BiFunction<T, EmiIngredient, Map<Bounds, Consumer<EmiIngredient>>> bounds;
+
     public EmiStorageGhostDragDropHandler() {
-        super(
-            screen -> {
-                List<Slot> slots = new ArrayList<>();
+        this.bounds = (screen, ingredient) -> {
+            Map<Bounds, Consumer<EmiIngredient>> map = Maps.newHashMap();
+            if (ingredient.getEmiStacks().isEmpty()) {
+                return map;
+            }
+
+            ItemStack ghostStack = ingredient.getEmiStacks().getFirst().getItemStack();
+            if (!ghostStack.isEmpty()) {
+                FluidStack fluidStack = CapabilityHelper.getFromCapability(ghostStack, FluidStorage.ITEM,
+                        null, fluidHandler -> Optional.ofNullable(StorageUtil.findExtractableContent(fluidHandler, null)).map(FluidStack::new).orElse(FluidStack.EMPTY), FluidStack.EMPTY);
+                if (!fluidStack.isEmpty()) {
+                    screen.getUpgradeSettingsControl().getOpenTab().filter(tab -> tab instanceof PumpUpgradeTab.Advanced).map(PumpUpgradeTab.Advanced.class::cast).ifPresent(pumpUpgradeTab -> {
+                        addFluidTargets(pumpUpgradeTab, fluidStack, map);
+                    });
+                }
                 screen.getMenu().getOpenContainer().ifPresent(c -> c.getSlots().forEach(s -> {
-                    if (s instanceof IFilterSlot) {
-                        slots.add(s);
+                    if (s instanceof IFilterSlot && s.mayPlace(ghostStack)) {
+                        map.put(
+                                new Bounds(screen.getLeftX() + s.x, screen.getTopY() + s.y, 18, 18),
+                                (i) -> {
+                                    PacketDistributor.sendToServer(new SetGhostSlotPayload(ghostStack, s.index));
+                                });
                     }
                 }));
-                return slots;
-            },
-            (screen, slot, ingredient) -> {
-                List<EmiStack> stacks = ingredient.getEmiStacks();
-                if (stacks.size() != 1)
-                    return;
-
-                ItemStack stack = stacks.get(0).getItemStack();
-                if (slot.mayPlace(stack)) {
-                    PacketDistributor.sendToServer(new SetGhostSlotPayload(stack, slot.index));
-                }
+            } else if (ingredient instanceof FluidEmiStack fluidStack) {
+                screen.getUpgradeSettingsControl().getOpenTab().filter(tab -> tab instanceof PumpUpgradeTab.Advanced).map(PumpUpgradeTab.Advanced.class::cast).ifPresent(pumpUpgradeTab -> {
+                    Fluid fluid = fluidStack.getKeyOfType(Fluid.class);
+                    addFluidTargets(pumpUpgradeTab, new FluidStack(fluid, 1), map);
+                });
             }
-        );
+            return map;
+        };
+    }
+
+    @Override
+    public boolean dropStack(T screen, EmiIngredient stack, int x, int y) {
+        Map<Bounds, Consumer<EmiIngredient>> bounds = this.bounds.apply(screen, stack);
+        for (Bounds b : bounds.keySet()) {
+            if (b.contains(x, y)) {
+                bounds.get(b).accept(stack);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public void render(T screen, EmiIngredient dragged, GuiGraphics draw, int mouseX, int mouseY, float delta) {
+        EmiDrawContext context = EmiDrawContext.wrap(draw);
+        for (Bounds b : this.bounds.apply(screen, dragged).keySet()) {
+            context.fill(b.x(), b.y(), b.width(), b.height(), 0x8822BB33);
+        }
+    }
+
+    private void addFluidTargets(PumpUpgradeTab.Advanced pumpUpgradeTab, FluidStack ghostFluid, Map<Bounds, Consumer<EmiIngredient>> map) {
+        List<Position> slotTopLeftPositions = pumpUpgradeTab.getFluidFilterControl().getSlotTopLeftPositions();
+        AtomicInteger slot = new AtomicInteger();
+        for (slot.set(0); slot.get() < slotTopLeftPositions.size(); slot.incrementAndGet()) {
+            Position position = slotTopLeftPositions.get(slot.get());
+            final int slotIndex = slot.get();
+            map.put(
+                    new Bounds(position.x(), position.y(), 17, 17),
+                    ingredient -> pumpUpgradeTab.getFluidFilterControl().setFluid(slotIndex, ghostFluid));
+        }
     }
 }
