f3a3b5b84fe9823d659cd7da0748c768187058db
Salandora <salandora@gmail.com>
2025-03-28 22:11:49 +0100

    [Fix]: Fix a bug where Controller blocks would randomly stop inserting items

--- src/main/java/net/p3pp3rf1y/sophisticatedcore/controller/ControllerBlockEntityBase.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedcore/controller/ControllerBlockEntityBase.java
@@ -648,7 +648,7 @@ public abstract class ControllerBlockEntityBase extends BlockEntity implements I
 			}
 		}
 
-		return (insertIntoAnyEmpty ? insertIntoStorages(emptySlotsStorages, resource, remaining, ctx, false) : maxAmount - remaining);
+		return (insertIntoAnyEmpty ? (maxAmount - remaining) + insertIntoStorages(emptySlotsStorages, resource, remaining, ctx, false) : maxAmount - remaining);
 	}
 
 	private long insertIntoStoragesThatMatchStack(ItemVariant resource, long maxAmount, ItemStackKey stackKey, @Nullable TransactionContext ctx) {
