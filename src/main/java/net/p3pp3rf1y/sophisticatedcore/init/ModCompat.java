b1a562a293f437f18d31a0656e0744e08b075506
Salandora <salandora@gmail.com>
2025-04-22 19:30:48 +0200

    [Compatibility]: Moved Litematica compatibility in to a new compatibility mod

--- src/main/java/net/p3pp3rf1y/sophisticatedcore/init/ModCompat.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedcore/init/ModCompat.java
@@ -3,14 +3,12 @@ package net.p3pp3rf1y.sophisticatedcore.init;
 import net.p3pp3rf1y.sophisticatedcore.compat.CompatInfo;
 import net.p3pp3rf1y.sophisticatedcore.compat.CompatModIds;
 import net.p3pp3rf1y.sophisticatedcore.compat.CompatRegistry;
-import net.p3pp3rf1y.sophisticatedcore.compat.audioplayer.AudioPlayerCompat;
 import net.p3pp3rf1y.sophisticatedcore.compat.craftingtweaks.CraftingTweaksCompat;
 //import net.p3pp3rf1y.sophisticatedcore.compat.curios.CuriosCompat;
 import net.p3pp3rf1y.sophisticatedcore.compat.emi.EmiCompat;
 //import net.p3pp3rf1y.sophisticatedcore.compat.inventorysorter.InventorySorterCompat;
 //import net.p3pp3rf1y.sophisticatedcore.compat.itemborders.ItemBordersCompat;
 import net.p3pp3rf1y.sophisticatedcore.compat.jei.JeiCompat;
-import net.p3pp3rf1y.sophisticatedcore.compat.litematica.LitematicaCompat;
 import net.p3pp3rf1y.sophisticatedcore.compat.rei.REICompat;
 import net.p3pp3rf1y.sophisticatedcore.compat.trinkets.TrinketsCompat;
 
@@ -32,8 +30,6 @@ public class ModCompat {
 		// Fabric addition
 		registry.registerCompat(new CompatInfo(CompatModIds.EMI, null), () -> new EmiCompat());
 		registry.registerCompat(new CompatInfo(CompatModIds.REI, null), () -> new REICompat());
-		registry.registerCompat(new CompatInfo(CompatModIds.LITEMATICA, null), () -> new LitematicaCompat());
-		registry.registerCompat(new CompatInfo(CompatModIds.AUDIOPLAYER, null), () -> new AudioPlayerCompat());
 		registry.registerCompat(new CompatInfo(CompatModIds.TRINKETS, null), () -> new TrinketsCompat());
 	}
 }
