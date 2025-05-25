820f23c2816539199e2d58be87d24597c2443a3d
Salandora <salandora@gmail.com>
2025-05-25 22:13:48 +0200

    [Fix]: Fix a bug with the hopper mixin causing hoppers to have a higher reach

--- src/main/java/net/p3pp3rf1y/sophisticatedcore/mixin/common/HopperBlockEntityMixin.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedcore/mixin/common/HopperBlockEntityMixin.java
@@ -1,13 +1,11 @@
 package net.p3pp3rf1y.sophisticatedcore.mixin.common;
 
-import com.llamalad7.mixinextras.sugar.Local;
 import net.fabricmc.fabric.api.transfer.v1.item.InventoryStorage;
 import net.fabricmc.fabric.api.transfer.v1.item.ItemVariant;
 import net.fabricmc.fabric.api.transfer.v1.storage.Storage;
 import net.fabricmc.fabric.api.transfer.v1.storage.StorageUtil;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
-import net.minecraft.world.Container;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntitySelector;
 import net.minecraft.world.level.Level;
@@ -22,6 +20,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.util.Collections;
 import java.util.List;
 
 @Mixin(HopperBlockEntity.class)
@@ -30,14 +29,11 @@ public class HopperBlockEntityMixin {
 	private Direction facing;
 
 	@Inject(
-			at = @At(
-					value = "INVOKE_ASSIGN",
-					target = "Lnet/minecraft/world/level/block/entity/HopperBlockEntity;getAttachedContainer(Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/entity/HopperBlockEntity;)Lnet/minecraft/world/Container;"
-			),
 			method = "ejectItems",
+			at = @At(value = "HEAD"),
 			cancellable = true
 	)
-	private static void sophisticatedCore_ejectItems(Level world, BlockPos pos, HopperBlockEntity blockEntity, CallbackInfoReturnable<Boolean> cir, @Local Container container) {
+	private static void sophisticatedCore_ejectItems(Level world, BlockPos pos, HopperBlockEntity blockEntity, CallbackInfoReturnable<Boolean> cir) {
 		Direction direction = ((HopperBlockEntityMixin) (Object) blockEntity).facing;
 		Vec3 searchPos = new Vec3(pos.getX() + direction.getStepX() + 0.5, pos.getY() + direction.getStepY() + 0.5, pos.getZ() + direction.getStepZ() + 0.5);
 		List<Entity> list = world.getEntities(
@@ -46,61 +42,62 @@ public class HopperBlockEntityMixin {
 						searchPos.x() - (double)0.5F, searchPos.y() - (double)0.5F, searchPos.z() - (double)0.5F,
 						searchPos.x() + (double)0.5F, searchPos.y() + (double)0.5F, searchPos.z() + (double)0.5F
 				),
-				EntitySelector.CONTAINER_ENTITY_SELECTOR
+				EntitySelector.ENTITY_STILL_ALIVE
 		);
 		if (list.isEmpty()) {
 			return;
 		}
 
-		Entity entity = list.get(world.random.nextInt(list.size()));
-		Storage<ItemVariant> target = Capabilities.ItemHandler.ENTITY_AUTOMATION.find(entity, direction.getOpposite());
-
-		if (target != null) {
-			long moved = StorageUtil.move(
-					InventoryStorage.of(blockEntity, direction),
-					target,
-					iv -> true,
-					1,
-					null
-			);
-			cir.setReturnValue(moved == 1);
+		Collections.shuffle(list);
+		for (Entity entity : list) {
+			Storage<ItemVariant> target = Capabilities.ItemHandler.ENTITY_AUTOMATION.find(entity, direction.getOpposite());
+			if (target != null) {
+				long moved = StorageUtil.move(
+						InventoryStorage.of(blockEntity, direction),
+						target,
+						iv -> true,
+						1,
+						null
+				);
+				cir.setReturnValue(moved == 1);
+				return;
+			}
 		}
 	}
 
 	@Inject(
-			at = @At(
-					value = "INVOKE_ASSIGN",
-					target = "Lnet/minecraft/world/level/block/entity/HopperBlockEntity;getSourceContainer(Lnet/minecraft/world/level/Level;Lnet/minecraft/world/level/block/entity/Hopper;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/state/BlockState;)Lnet/minecraft/world/Container;"
-			),
 			method = "suckInItems",
+			at = @At(value = "HEAD"),
 			cancellable = true
 	)
-	private static void sophisticatedCore_suckInItems(Level world, Hopper hopper, CallbackInfoReturnable<Boolean> cir, @Local Container container) {
+	private static void sophisticatedCore_suckInItems(Level world, Hopper hopper, CallbackInfoReturnable<Boolean> cir) {
 		Vec3 searchPos = new Vec3(hopper.getLevelX(), hopper.getLevelY() + 1.0F, hopper.getLevelZ());
 		List<Entity> list = world.getEntities(
 				(Entity)null,
 				new AABB(
 						searchPos.x() - (double)0.5F, searchPos.y() - (double)0.5F, searchPos.z() - (double)0.5F,
-						searchPos.x() + (double)0.5F, searchPos.y() + (double)0.5F + 1.0F, searchPos.z() + (double)0.5F
+						searchPos.x() + (double)0.5F, searchPos.y() + (double)0.5F, searchPos.z() + (double)0.5F
 				),
-				EntitySelector.CONTAINER_ENTITY_SELECTOR
+				EntitySelector.ENTITY_STILL_ALIVE
 		);
 		if (list.isEmpty()) {
 			return;
 		}
 
-		Entity entity = list.get(world.random.nextInt(list.size()));
-		Storage<ItemVariant> source = Capabilities.ItemHandler.ENTITY_AUTOMATION.find(entity, Direction.DOWN);
-
-		if (source != null) {
-			long moved = StorageUtil.move(
-					source,
-					InventoryStorage.of(hopper, Direction.UP),
-					iv -> true,
-					1,
-					null
-			);
-			cir.setReturnValue(moved == 1);
+		Collections.shuffle(list);
+		for (Entity entity : list) {
+			Storage<ItemVariant> source = Capabilities.ItemHandler.ENTITY_AUTOMATION.find(entity, Direction.DOWN);
+			if (source != null) {
+				long moved = StorageUtil.move(
+						source,
+						InventoryStorage.of(hopper, Direction.UP),
+						iv -> true,
+						1,
+						null
+				);
+				cir.setReturnValue(moved == 1);
+				return;
+			}
 		}
 	}
 }
