fd7065aacbbc4c4ccb73f6d218cd84ec01f4cf2b
Salandora <salandora@gmail.com>
2025-05-31 19:44:58 +0200

    [Fix]: Fixed tooltips not showing on scrollable containers

--- src/main/java/net/p3pp3rf1y/sophisticatedcore/mixin/client/AbstractContainerScreenMixin.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedcore/mixin/client/AbstractContainerScreenMixin.java
@@ -1,5 +1,6 @@
 package net.p3pp3rf1y.sophisticatedcore.mixin.client;
 
+import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.client.gui.GuiGraphics;
@@ -16,12 +17,12 @@ import net.p3pp3rf1y.sophisticatedcore.common.gui.StorageContainerMenuBase;
 import net.p3pp3rf1y.sophisticatedcore.extensions.client.gui.screens.inventory.SophisticatedAbstractContainerScreen;
 import net.p3pp3rf1y.sophisticatedcore.util.MixinHelper;
 import org.jetbrains.annotations.Nullable;
+import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(AbstractContainerScreen.class)
@@ -126,6 +127,19 @@ public abstract class AbstractContainerScreenMixin<T extends AbstractContainerMe
 				widget.render(guiGraphics, mouseX, mouseY, partialTick);
 			}
 		}
+	}
+
+	@WrapWithCondition(
+			method = "render",
+			at = @At(
+					value = "FIELD",
+					target = "Lnet/minecraft/client/gui/screens/inventory/AbstractContainerScreen;hoveredSlot:Lnet/minecraft/world/inventory/Slot;",
+					opcode = Opcodes.PUTFIELD,
+					ordinal = 0
+			)
+	)
+	private boolean sophisticatedCore$bypassHoveredSlotReset(AbstractContainerScreen<T> instance, Slot newSlot) {
+		return !sophisticatedCore$isStorageScreen;
 	}
 
 	@Override
