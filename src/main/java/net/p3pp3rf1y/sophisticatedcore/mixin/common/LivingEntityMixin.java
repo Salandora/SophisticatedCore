516ced3e4c08529a0c060a3b067a02fea88baec9
Salandora <salandora@gmail.com>
2025-08-23 22:40:30 +0200

    [Fix]: Fixed incompatibility with double slabs

--- src/main/java/net/p3pp3rf1y/sophisticatedcore/mixin/common/LivingEntityMixin.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedcore/mixin/common/LivingEntityMixin.java
@@ -1,12 +1,8 @@
 package net.p3pp3rf1y.sophisticatedcore.mixin.common;
 
-import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.particles.ParticleOptions;
 import net.minecraft.server.level.ServerLevel;
@@ -21,6 +17,11 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import net.p3pp3rf1y.sophisticatedcore.event.common.LivingEntityEvents;
 import net.p3pp3rf1y.sophisticatedcore.util.MixinHelper;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -34,12 +35,18 @@ public abstract class LivingEntityMixin extends Entity {
         super(entityType, world);
     }
 
-    @Inject(method = "dropAllDeathLoot", at = @At("HEAD"))
+    @Inject(
+			method = "dropAllDeathLoot",
+			at = @At("HEAD")
+	)
     private void sophisticatedcore$captureDrops(DamageSource damageSource, CallbackInfo ci) {
         sophisticatedCaptureDrops(new ArrayList<>());
     }
 
-    @Inject(method = "dropAllDeathLoot", at = @At(value = "RETURN"))
+    @Inject(
+			method = "dropAllDeathLoot",
+			at = @At(value = "RETURN")
+	)
     private void sophisticatedcore$dropCapturedDrops(DamageSource damageSource, CallbackInfo ci) {
         Collection<ItemEntity> drops = this.sophisticatedCaptureDrops(null);
 
@@ -55,13 +62,28 @@ public abstract class LivingEntityMixin extends Entity {
             drops.forEach(e -> level().addFreshEntity(e));
     }
 
-    @Inject(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/Entity;tick()V"))
+    @Inject(
+			method = "tick",
+			at = @At(
+					value = "INVOKE",
+					target = "Lnet/minecraft/world/entity/Entity;tick()V"
+			)
+	)
     private void sophisticatedcore$tick(CallbackInfo ci) {
         LivingEntityEvents.TICK.invoker().onLivingEntityTick(MixinHelper.cast(this));
     }
 
-	@WrapWithCondition(method = "checkFallDamage", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/level/ServerLevel;sendParticles(Lnet/minecraft/core/particles/ParticleOptions;DDDIDDDD)I"))
-	public boolean sophisticatedcore$addLandingEffects(ServerLevel level, ParticleOptions type, double posX, double posY, double posZ, int particleCount, double xOffset, double yOffset, double zOffset, double speed, double y, boolean onGround, BlockState state, BlockPos pos) {
-		return !state.addLandingEffects(level, pos, state, MixinHelper.cast(this), particleCount);
+	@WrapOperation(
+			method = "checkFallDamage",
+			at = @At(
+					value = "INVOKE",
+					target = "Lnet/minecraft/server/level/ServerLevel;sendParticles(Lnet/minecraft/core/particles/ParticleOptions;DDDIDDDD)I"
+			)
+	)
+	public <T extends ParticleOptions> int sophisticatedcore$addLandingEffects(ServerLevel level, T type, double posX, double posY, double posZ, int particleCount, double xOffset, double yOffset, double zOffset, double speed, Operation<Integer> original, @Local(argsOnly = true) BlockState state, @Local(argsOnly = true) BlockPos pos) {
+		if (!state.addLandingEffects(level, pos, state, MixinHelper.cast(this), particleCount)) {
+			return original.call(level, type, posX, posY, posZ, particleCount, xOffset, yOffset, zOffset, speed);
+		}
+		return particleCount;
 	}
 }
