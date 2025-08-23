a0ecd32b5a8b78842bb257b7c9dbf4f850702685
Salandora <salandora@gmail.com>
2025-08-23 22:40:30 +0200

    [Fix]: Fixed incompatibility with double slabs

--- src/main/java/net/p3pp3rf1y/sophisticatedcore/mixin/client/MinecraftMixin.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedcore/mixin/client/MinecraftMixin.java
@@ -1,10 +1,7 @@
 package net.p3pp3rf1y.sophisticatedcore.mixin.client;
 
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
-
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.particle.ParticleEngine;
@@ -12,6 +9,9 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.HitResult;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
 
 import javax.annotation.Nullable;
 
@@ -25,11 +25,17 @@ public class MinecraftMixin {
 	@Nullable
 	public HitResult hitResult;
 
-    @Redirect(method = "continueAttack", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/particle/ParticleEngine;crack(Lnet/minecraft/core/BlockPos;Lnet/minecraft/core/Direction;)V"))
-    private void sophisticatedcore$addBlockHitEffects(ParticleEngine manager, BlockPos pos, Direction side) {
+    @WrapOperation(
+			method = "continueAttack",
+			at = @At(
+					value = "INVOKE",
+					target = "Lnet/minecraft/client/particle/ParticleEngine;crack(Lnet/minecraft/core/BlockPos;Lnet/minecraft/core/Direction;)V"
+			)
+	)
+    private void sophisticatedcore$addBlockHitEffects(ParticleEngine instance, BlockPos pos, Direction side, Operation<Void> original) {
         BlockState state = level.getBlockState(pos);
-        if (!state.addHitEffects(level, this.hitResult, manager)) {
-            manager.crack(pos, side);
+        if (!state.addHitEffects(level, this.hitResult, instance)) {
+            original.call(instance, pos, side);
         }
     }
 }
